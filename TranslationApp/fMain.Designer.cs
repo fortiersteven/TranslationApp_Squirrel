72c1fad5ef0abcb8ca04e5e3e8ee87ead7807e87
Stewie <fortiersteven1@gmail.com>
2022-07-29 15:26:27 -0400

    redo 1.0.0

--- TranslationApp/fMain.Designer.cs
+++ TranslationApp/fMain.Designer.cs
@@ -92,7 +92,6 @@ namespace TranslationApp
             this.lNbToDoSect = new System.Windows.Forms.Label();
             this.bRefresh = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.menuStripMain.SuspendLayout();
             this.tcType.SuspendLayout();
             this.tabType1.SuspendLayout();
@@ -712,22 +711,11 @@ namespace TranslationApp
             this.button1.Text = "Potato button";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.button2.Location = new System.Drawing.Point(595, 86);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(144, 23);
-            this.button2.TabIndex = 38;
-            this.button2.Text = "New feature";
-            this.button2.UseVisualStyleBackColor = false;
-            // 
             // fMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(751, 737);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.bRefresh);
             this.Controls.Add(this.panelNb2);
@@ -847,7 +835,6 @@ namespace TranslationApp
         private System.Windows.Forms.Button bRefresh;
         private System.Windows.Forms.ToolStripMenuItem updateAppToolStripMenuItem;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
 
